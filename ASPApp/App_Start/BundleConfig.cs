5e6b6b7fbe07de391258fb92fda7473171cc5a51
James <jmcredmond5@hotmail.com>
2018-11-26 18:13:17 +0100

    import styles and scripts

---
+++ ASPApp/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ASPApp.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/Scripts/jquery-3.3.1.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/Scripts/jquery-3.3.1.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js"));
+        }
+    }
+}
--- ASPApp/Global.asax.cs
+++ ASPApp/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ASPApp.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ASPApp
@@ -14,6 +16,7 @@ namespace ASPApp
             AreaRegistration.RegisterAllAreas();
 
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
